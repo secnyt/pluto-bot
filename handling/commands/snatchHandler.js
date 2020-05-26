3889848c9a120d7d7163610efe93ae6defc081fc
secnyt <47509759+secnyt@users.noreply.github.com>
2020-05-25 21:16:57 -0500

    change how snatches are handled
    Snatch class, condensed snatching, etc.

--- handling/commands/snatchHandler.js
+++ handling/commands/snatchHandler.js
@@ -1,7 +1,22 @@
-const stuff = require('./../../pluto.js');
+//const stuff = require('./../../pluto.js');
 const package = require('./../../package.json');
+const fs = require('fs');
 
-var snh = {}
+var allSnatches = JSON.parse(fs.readFileSync('./storage/snatches/snatches.json'));
+
+var snh = {};
+
+class Snatch {
+    constructor(k, v, p){
+        this.key = k;
+        this.val = v;
+        this.perms = p;
+    }
+}
+
+var write = function(data){
+    fs.writeFileSync('./storage/snatches/snatches.json', JSON.stringify(data, null, 4));
+};
 
 snh.handleSnatch1 = function(msg, tbs, parameters, client, data){
     //console.log(tbs);
@@ -9,11 +24,11 @@ snh.handleSnatch1 = function(msg, tbs, parameters, client, data){
     switch(tbs){ 
         case "soh":
             if(data.hasTMNec){
-                stuff.er.TMP(msg);
+                //stuff.er.TMP(msg);
                 break;
             }
             if(!data.hasNec){
-                stuff.er.NP(msg);
+                //stuff.er.NP(msg);
                 break;
             }
             // Japanese
@@ -36,7 +51,7 @@ snh.handleSnatch1 = function(msg, tbs, parameters, client, data){
         case "smh":
 
             // coming soon
-            stuff.er.CS(msg);
+            //stuff.er.CS(msg);
             break;
 
         case "secnytplaylist":
@@ -84,19 +99,28 @@ snh.handleSnatch1 = function(msg, tbs, parameters, client, data){
 
 
 snh.handle = function(msg, client){
+    allSnatches = JSON.parse(fs.readFileSync('./storage/snatches/snatches.json'));
+    console.log(allSnatches);
+    var gid = msg.guild.id;
+    var gsn = allSnatches[gid];
+    console.log(gsn);
     let command = msg.content.split('/')[1];
     let parameters = command.split(" ");
     let c = parameters.splice(0, 1)[0];
     switch(c){
         case "s":
-            var toBreak = false;
+            //var toBreak = false;
 
+            // basically useless, though may be useful later 
             var errs = {
                 hasTMNec: false,
                 hasNec: true
             };
+
             // separate first parameter (to be snatched)
             let snatch = parameters.splice(0, 1)[0];
+
+            // is the user trying to create a Snatch (default no)
             let create = false;
             // organize and setup parameters
             
@@ -109,43 +133,103 @@ snh.handle = function(msg, client){
             if(snatch == "create"){
                 create = true;
             }
+            // ADD IN PERMISSION CHECKER (must have admin or higher) PLUS CAPTCHA SYSTEM
             if(create == false){
-                // japanese, spanish
-                var nec = [".j", ".s"];
-                // delete, DM
-                var opt = [".d", ".D"];
-                
-                // amounts of necessary and optional parameters
-                var amount = [0, 0];
-                // counts each type
-                
-                parameters.forEach(p => {
-                    if(nec.includes(p)){
-                        amount[0] += 1;
-                        if(amount[0] > 1){
-                            //stuff.er.TMP(msg);
-                            //toBreak = true;
-                            errs.hasTMNec = true;
-                        }
-                    }
-                    if(opt.includes(p)){
-                        amount[1] += 1;
-                    }
-                });
-                if(toBreak){
-                    break;
+                if(!gsn){
+                    gsn = {};
+                    allSnatches[gid] = gsn;
+                    write(allSnatches);
                 }
-                if(amount[0] < 1){
-                    //stuff.er.NP(msg);
-                    //break;
-                    errs.hasNec = false;
+                //console.log(Object.keys(gsn));
+                if(Object.keys(gsn).includes(snatch)){
+                    msg.channel.send(gsn[snatch].key);
                 }
-                snh.handleSnatch1(msg, snatch, parameters, client, errs);
-
-                break;
             }
             else if(create == true){
-                msg.channel.send('I work, great.');
+
+                let key;
+                let value;
+
+                console.log(parameters);
+                key = parameters[0];
+                console.log(key);
+                let d = false;
+
+                let flags = [];
+
+                for(var i of parameters){
+                    var w = parameters[i + 1];
+                    var wArray = w.split('');
+                    if((i + 1) < parameters.length){
+                        if(!d){
+                            if(!wArray[wArray.length - 1] == ":"){
+                                value += w;
+                            }
+                            else if(wArray[wArray.length - 1] == ":"){
+                                d = true;
+                                value += w;
+                            }
+                        }
+                        else {
+                            flags.push(w);
+                        }
+                    }
+                }
+
+                var perms = [];
+                var onDup = 'a';
+
+                flags.forEach((f, i) => {
+                    if(f == '.w'){
+                        perms.push('w');
+                        flags.splice(i, 1)
+                    }
+                    else if(f == '.a'){
+                        perms.push('w');
+                        flags.splice(i, 1)
+                    }
+                    else if(f == '.o'){
+                        onDup = 'o';
+                        flags.splice(i, 1)
+                    }
+                    else if(f == '.c'){
+                        onDup = 'c';
+                        flags.splice(i, 1)
+                    }
+                });
+                
+                var newSnatch = new Snatch(key, value, perms);
+
+                if(!gsn){
+                    gsn = {};
+                }
+
+                let toPush = true;
+
+                if(gsn[newSnatch.key]){
+                    switch(onDup){
+                        case 'a':
+                            if(gsn[newSnatch.key]){
+                                msg.channel.send('This key is already in use, or it is reserved.\nYou didn\'t specify an option in case of this happening, so by default, the creation is canceled.');
+                                toPush = false;
+                            }
+                            else {
+                                msg.channel.send(`The snatch \`${newSnatch.key}\` has been created with value \`${newSnatch.value}\`.`)
+                            }
+                            break;
+                        case 'c':
+                            msg.channel.send('This key is in use and the creation has been canceled.');
+                            toPush = false;
+                        case 'o':
+                            msg.channel.send('This key is in use, but has been overwritten.');
+                            toPush = true;
+                    }
+                }
+
+                gsn[newSnatch.key] = newSnatch;
+                allSnatches[gid] = gsn;
+
+                write(allSnatches);
             }
     }
 }
