afe988e492c856bdc049fbe73c4e533a56ca3714
secnyt <47509759+secnyt@users.noreply.github.com>
2020-08-30 13:04:30 -0500

    clean up snatch code

--- handling/commands/PlutoObjectNotation/snatchHandler.js
+++ handling/commands/PlutoObjectNotation/snatchHandler.js
@@ -24,104 +24,86 @@ var write = function(data){
 
 snh.handle = function(msg, client){
     try{
-        allSnatches = JSON.parse(fs.readFileSync('./storage/snatches/snatches.json'));
-        //console.log(allSnatches);
-        var gid = msg.guild.id;
-        var gsn = allSnatches[gid];
-        //console.log(gsn);
-        let parameters = msg.content.split(";");
+        var gid = msg.guild.id; // needed for guild-specific snatches
+
+        let parameters = msg.content.split(";"); // separates command into its specific parts
         
-        var command = parameters[0].split(" ")[1];
-        var key = parameters[1];
+        var command = parameters[0].split(" ")[1]; // the first part of the command needs to be split into two parts (since there are no semicolons between)
+        var key = parameters[1]; // the key of the snatch
+
         var flags = [];
-        if(parameters[3]){
-            flags = parameters[3].split(' ');
-            console.log(flags);
+        if(parameters[3]){ // if flags are specified,
+            flags = parameters[3].split(' '); // turn them into an array.
         }
-        parameters.forEach((v, i) => {
+        parameters.forEach((v, i) => { // trims all parameters
             parameters[i] = v.trim();
         });
         
 
-        switch(command){
+        switch(command){ // checks to see which function the user wants to use
             case "create":
             case "add":
-                var snatch = pon.compile(parameters[2], "vanilla");
-                if(!parameters[2]){
+                var snatch = pon.compile(parameters[2], "vanilla"); // compiles the PlutoObjectNotation
+                if(!parameters[2]){ // if the user didn't specify a snatch parameter
                     msg.channel.send("You are missing the snatch parameter! Make sure you are separating with a semicolon (;)");
                 }
-                else if(!parameters[2].trim().startsWith("s(\"")){
+                else if(!parameters[2].trim().startsWith("s(\"") || !parameters[2].trim().endsWith("\")")){ // if the user didn't use PlutoObjectNotation<String>
                     msg.channel.send("Your `snatch` parameter must be in PlutoObjectNotation<String> form. ex. `s(\"ABC 123.\")`");
                 }
-                else if(!snatch){
-                    msg.channel.send("You can't create an empty snatch!");
-                } else {
-                    var exists = true;
-                    MongoClient.connect(url, (err, db) => {
-                        if(err) console.error(err);
+                else if(!snatch){ // if the user had an empty PlutoObjectNotation and/or PlutoObjectNotation included forbidden characters.
+                    msg.channel.send("You can't create an empty snatch, or snatches with \"s!");
+                } else { // syntax = good
 
-                        var dbo = db.db('pluto-snatches');
-                        var get = {
-                            guild: gid
-                        }
-                        dbo.collection(" guilds").find(get).toArray((err, res) => {
-                            if(err) throw err;
-                            //console.log(res);
-                            if(!res){
-                                exists = false;
-                            }
-                        });
-                    });
-                    MongoClient.connect(url, (err, db) => {
+                    MongoClient.connect(url, (err, db) => { // connect to DB
                         if(err) throw err;
 
-                        var dbo = db.db('pluto-snatches');
-                        var ins = {
+                        var dbo = db.db('pluto-snatches'); // connect to snatches DB
+                        var ins = { // snatch class
                             key: parameters[1],
                             snatch: snatch,
                             reserved: false,
                             permissions: []
                         };
-                        
-                        if(!exists){
-                            dbo.createCollection(gid, (err, res) => {
-                                if(err) throw err;
-                                //console.log('collection created');
-                            })
-                        }
-                        var get = {
+
+                        var get = { // query for seeing if a snatch with the same key exists
                             key: parameters[1]
                         }
-                        snatchExists = false;
-                        dbo.collection(gid).find(get).toArray((err, res) => {
+                        snatchExists = false; // default does not exist
+                        dbo.collection(gid).find(get).toArray((err, res) => { // checks for snatches with the same name
                             if(err) console.error(err);
-                            snatchExists = res[0];
-                            console.log(snatchExists);
-                            if(!snatchExists){
-                                console.log('abc');
+                            snatchExists = res[0]; // sets snatchExists to the first snatch with the same name, if there isn't one, undefined.
+                            if(!snatchExists){ // if undefined, return false
                                 snatchExists = false;
+                            } else { 
+                                snatchExists = true;
                             }
-                            if(flags.includes('.o') || flags.includes('..overwrite') || flags.includes('..override')){
-                                dbo.collection(gid).findOneAndReplace(get, ins, (err, res) => {
-                                    if(err) throw err;
-                                    //console.log('snatch inserted');
-                                    msg.channel.send('Your snatch has been processed and updated.')
-                                    db.close();
-                                })
-                            } else if(snatchExists){
-                                msg.channel.send('A snatch with this key exists. Please specify in a flag whether you would like to overwrite. ```/s command; key; PON<string>; **flags**\n```')
-                            } else {
-                                console.log(snatchExists);
+                            if(flags.includes('.o') || flags.includes('..overwrite') || flags.includes('..override')){ // is there a flag for overriding?
+                                if(snatchExists){ // if there already exists && overwrite
+                                    dbo.collection(gid).findOneAndReplace(get, ins, (err, res) => { // find a replace the already existing
+                                        if(err) throw err;
+                                        msg.channel.send('Your snatch has been processed and updated.')
+                                        db.close();
+                                    });
+                                }
+                                else { // doesnt exist && overwrite
+                                    dbo.collection(gid).insertOne(ins, (err, res) => { // just insert
+                                        if(err) throw err;
+                                        msg.channel.send('Your snatch has been processed and updated.')
+                                        db.close();
+                                    });
+                                }
+                            } else if(snatchExists){ // no overwrite flag, but exists => no insert
+                                msg.channel.send('A snatch with this key exists. Please specify in a flag whether you would like to overwrite.')
+                            } else { // no overwrite flag, but doesn't exist => insert
                                 dbo.collection(gid).insertOne(ins, (err, res) => {
                                     if(err) throw err;
-                                    //console.log('snatch inserted');
                                     msg.channel.send('Your snatch has been processed and uploaded.')
                                     db.close();
                                 }) 
                             }
                         });
                         
-                        if(flags.includes('.d') || flags.includes('..delete')){
+                        if(flags.includes('.d') || flags.includes('..delete')){ // delete message if a delete flag existed
                             msg.delete();
                         }
 
@@ -132,25 +114,23 @@ snh.handle = function(msg, client){
             case "snatch":
             case "use":
                 try {
-                    var key = parameters[1];
-
+                    if(parameters[2]){ // flags are in the 2nd index since there is one less parameter 
+                        flags = parameters[2].split(' '); // separate flags
+                    }
                     MongoClient.connect(url, (err, db) => {
                         var dbo = db.db('pluto-snatches');
-                        var get = {
+                        var get = { // query to find snatch
                             key: key
                         };
 
-                        dbo.collection(gid).find(get).toArray((err, res) => {
-                            try{
-                                console.log(res);
-                                console.log(res[0]);
-                                res = res[0].snatch;
+                        dbo.collection(gid).find(get).toArray((err, res) => { // find query and return array
+                            try {
+                                res = res[0].snatch; // find first match and get the snatch value
                                 if(err){
                                     console.error(err);
                                     msg.channel.send(`This snatch does (most likely) not exist. If you believe this was in error, please join the support server for help. (\`${require('./../../../package.json').prefix}support\`)`);
-                                };
-                                msg.channel.send(res);
-                                console.log(res);
+                                }
+                                msg.channel.send(res); // send matching snatch
                                 db.close();
                             } catch(err){
                                 console.error(err);
@@ -165,8 +145,8 @@ snh.handle = function(msg, client){
                     }
                 }
                 break;
-            default:
-                msg.channel.send("I don't understand what you are trying to do! Parameter 1, ```/s **command**; key; PON<string>; flags\n```");
+            default: // didn't specify a working command
+                msg.channel.send("I don't understand what you are trying to do!");
                 break;
         }
     } catch(err){
