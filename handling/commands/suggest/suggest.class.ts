633099f99ef68ed67f915d52cd28b1bc4354fe12
secnyt <47509759+secnyt@users.noreply.github.com>
2020-09-22 06:51:47 -0500

    rewrite suggest

--- handling/commands/suggest/suggest.class.ts
+++ handling/commands/suggest/suggest.class.ts
@@ -1,7 +1,9 @@
-class Suggestion{
+class Suggest{
   constructor(private c: string, private g: string, private u: any) { };
 
   get content() { return this.c };
   get guild() { return this.g };
   get user() { return this.u };
 }
+
+module.exports = Suggest;
