c7a77239b9ab75a179c237a7be42bd90df8136b3
secnyt <47509759+secnyt@users.noreply.github.com>
2020-09-21 21:17:08 -0500

    suggest class stuffs

---
+++ handling/commands/suggest/suggest.class.ts
@@ -0,0 +1,7 @@
+class Suggestion{
+  constructor(private c: string, private g: string, private u: any) { };
+
+  get content() { return this.c };
+  get guild() { return this.g };
+  get user() { return this.u };
+}
---
+++ handling/commands/suggest/suggest.handler.ts
@@ -0,0 +1,8 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+let suggestHandler: any = {};
+
+suggestHandler.handle = (msg: any, client: any) => {
+  
+}
