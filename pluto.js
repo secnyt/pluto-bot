3a6bc3cf2cc81b13245fc2e1a2d539a941d9934f
secnyt <47509759+secnyt@users.noreply.github.com>
2021-02-03 21:06:55 -0600

    Revert "update pluto.js for new rewrite"
    This reverts commit 88089770cbd1444567126d80294e3506e76aed8d.

--- pluto.js
+++ pluto.js
@@ -1,20 +1,3 @@
-/*  This file is part of Pluto.
- *
- *  Pluto is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
-
 // JSONs
 var { token } = require('./auth.json');
 var { prefix } = require('./package.json');
@@ -34,9 +17,13 @@ client.on('ready', () => {
 client.on('message', msg => {
     if(msg.channel.type == 'text' && !msg.author.bot){ // make sure message is eligible to be read
         if(msg.content.startsWith(prefix)){
-            require('./reworked-handling/CommandHandler').handle(msg);
+            require('./handling/command.handler').handle(msg, client);
         }
     }
+});
+
+client.on('guildMemberAdd', member => {
+    require('./handling/joinHandler.js').handle(client, member);
 });
 
 function setup(){
