b384f7bbe46c6b94afe75a954419a2ed4c029939
secnyt <47509759+secnyt@users.noreply.github.com>
2021-03-23 19:39:59 -0500

    Remove Slash Commands

--- src/main/pluto/handlers/SlashCommandHandler.ts
+++
@@ -1,7 +0,0 @@
-export default class SlashCommandHandler {
-    static async handle (interaction: any) {
-        console.log(interaction)
-        console.log(interaction.data)
-        return interaction
-    }
-}
